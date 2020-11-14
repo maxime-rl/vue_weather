3b820c05f75471d7163c0e5287d185b2a7e0a749
maxime-rl <maxime.robil.lepretre@gmail.com>
2020-11-14 03:11:21 +0100

    call API and integration

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home msg="App Météo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+#app{
+  height: 100vh;
+  font-family: 'Lato', sans-serif;
+  background-color: #fdfdfd;
 }
+h1,h2,h3,h4,h5,h6{
+      color: #3d3d3d;
+}
+h1{
+  font-size: 2rem;
+  font-weight: 700;
+}
+
+li{
+  list-style: none;
+};
+
 </style>
